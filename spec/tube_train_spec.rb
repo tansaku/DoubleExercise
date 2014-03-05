2f029c824756a0bab2ea48f65ebfc5c5337d121e
Sam Joseph <tansaku@gmail.com>
2014-03-05 16:37:52 +0000

    fixing the spec

--- spec/tube_train_spec.rb
+++ spec/tube_train_spec.rb
@@ -3,6 +3,6 @@ require 'tube_train'
 describe 'TubeTrain' do
 	let(:tube_train){TubeTrain.new}
 	it 'should be in a station' do
-      expect(tube_train).respond_to :current_station 
+      tube_train.should respond_to :current_station 
 	end
 end
