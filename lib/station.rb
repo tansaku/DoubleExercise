976967bd39f93e1fe962217770c45c7de3623622
Sam Joseph <tansaku@gmail.com>
2014-03-12 17:03:24 +0000

    implementing passenger counts

--- lib/station.rb
+++ lib/station.rb
@@ -1,8 +1,15 @@
 class Station 
   attr_reader :trains
-  
+
 	def initialize trains
     @trains = trains
 	end
 
+  def passenger_count
+    sum = 0
+    @trains.each do |t|
+      sum += t.passenger_count
+    end
+    sum
+  end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,11 +2,16 @@ require 'station'
 
 describe 'station' do 
   let(:station) {Station.new trains}
-  let(:trains){[(double :train), (double :train)]}
+  let(:trains){[train1, train2]}
+  let(:train1){double :train, passenger_count: 3}
+  let(:train2){double :train, passenger_count: 143}
   it '#initialize' do
     station
   end
   it '#trains' do
     station.trains.should eq trains
+  end
+  it '#passenger_count' do
+    station.passenger_count.should eq 146
   end
 end
